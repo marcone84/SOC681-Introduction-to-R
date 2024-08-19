2f29d9f9475abea04385d72675a57b078b45aae9
Marcus <marcone84@gmail.com>
2024-08-19 11:44:12 -0400

    redoing materials

--- 2024/course-materials/code/l1_problem_1.R
+++
@@ -1,9 +0,0 @@
-# Fix the code on lines 3-6
-
-calculate_mean <- function() {
-    mean <- sum(x) / len(x)
-    return()
-}}
-
-# This line should run once the code works. What is your answer?
-round(calculate_mean(runif(1E6)),2)
